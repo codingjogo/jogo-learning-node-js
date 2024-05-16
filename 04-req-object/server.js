4806e7b29187fc23d82d703ad8316239adc1ef7d
Jogo <138952391+codingjogo@users.noreply.github.com>
2024-05-16 19:17:08 +0800

    added ".env" in gitignore file and created 04-req-object

---
+++ 04-req-object/server.js
@@ -0,0 +1,20 @@
+import http from 'http';
+// open a PORT for server
+const PORT = process.env.PORT;
+
+// creates a server
+const server = http.createServer((req,res) => {
+    // res.write('Hello World WRITE');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+
+    // instead separating, do this:
+    res.writeHead(404, {'Content-Type': 'text/html'})
+
+    res.end(JSON.stringify({ message: 'ERROR MESSAGE GOES HERE'}));
+})
+
+// listen to server
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
