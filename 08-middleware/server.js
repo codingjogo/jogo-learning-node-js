8714da981312c939461196618fffc161fe186cfd
Joshua I. Gonzales <138952391+codingjogo@users.noreply.github.com>
2024-06-08 10:29:07 +0800

    Created /08-middleware and learned it
    new file:   .gitignore
    new file:   example.js
    new file:   package-lock.json
    new file:   package.json
    new file:   public/about.html
    new file:   public/index.html
    new file:   server.js

---
+++ 08-middleware/server.js
@@ -0,0 +1,61 @@
+import { createServer } from "http";
+const PORT = process.env.PORT;
+
+const users = [
+	{ id: 1, name: "Jogo" },
+	{ id: 2, name: "Jane Doe" },
+	{ id: 3, name: "Mak Sheesh" },
+];
+
+// Logger Middleware
+const logger = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+}
+
+// creates a server
+const server = createServer((req, res) => {
+	// init next function for looger middleware
+	const next = () => {
+		if (req.url === "/api/users" && req.method === "GET") {
+		  res.setHeader("Content-Type", "application/json");
+		  res.write(JSON.stringify(users));
+		  res.end();
+		} else if (
+		  req.url.match(/\/api\/users\/([0-9]+)/) &&
+		  req.method === "GET"
+		) {
+		  const urlSplit = req.url.split("/"); // input: 1 output: [ '', 'api', 'users', '1' ]
+		  const id = urlSplit[3]; // gets the 3rd index which is "1";
+		  const user = users.find((user) => user.id === parseInt(id)); // returns object that matches the ID from request URL
+		  res.setHeader("Content-Type", "application/json");
+	
+		  // if user exists
+		  if (user) {
+			console.log(`ID user: ${id}`);
+			console.log(`User: ${user}`);
+			res.write(JSON.stringify(user));
+		  } else {
+			console.error(`User ID:${id} not found`);
+			res.statusCode = 404;
+			res.write(JSON.stringify({ message: "User not found" }));
+		  }
+	
+		  res.end();
+		} else {
+		  res.setHeader("Content-Type", "application/json");
+		  res.statusCode = 404;
+		  res.write(JSON.stringify({ message: "Route not found" }));
+		  res.end();
+		}
+	  };
+	
+	  // invoke logger middleware after next() function
+	  logger(req, res, next);
+	
+});
+
+// listen to server
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
